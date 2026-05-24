43eacd8d3297edd8eddc9ffa34a12fa6059bd184
boinhaTI <grafboinha@gmail.com>
2026-05-24 20:36:13 -0300

    Adicionando componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <header className="
+    bg-white
+    p-6
+    rounded-3xl
+    shadow-xl
+    mb-6
+    ">
+
+      <h1 className="
+      text-4xl
+      font-bold
+      text-orange-600
+      ">
+        🛒 Mercearia Central
+      </h1>
+
+      <p className="text-gray-500 mt-2">
+        Controle de produtos e estoque
+      </p>
+
+    </header>
+  );
+}
